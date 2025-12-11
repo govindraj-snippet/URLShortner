db6857d114c7813c3d851a4d863a268206529b9a
govindraj-snippet <2023bit035@sggs.ac.in>
2025-12-11 23:30:56 +0530

    completed models part of the project

--- url-shortner/src/main/java/com/url/shortner/models/ClickEvent.java
+++ url-shortner/src/main/java/com/url/shortner/models/ClickEvent.java
@@ -1,4 +1,23 @@
 package com.url.shortner.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
 public class ClickEvent {
+    private Long id ;
+    private LocalDateTime clickDate ;
+   // private LocalDateTime clickDate ;
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private UrlMapping urlMapping ;
+
+
+
+
 }
--- url-shortner/src/main/java/com/url/shortner/models/UrlMapping.java
+++ url-shortner/src/main/java/com/url/shortner/models/UrlMapping.java
@@ -1,4 +1,22 @@
 package com.url.shortner.models;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String originalUrl ;
+    private String shortUrl ;
+    private int clickCount = 0  ;
+    private LocalDateTime createdTime ;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user ;
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents ;
 }
