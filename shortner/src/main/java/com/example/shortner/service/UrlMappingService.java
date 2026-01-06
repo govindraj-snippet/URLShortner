846e1d9d7307d8ff627408651e390d3872db26c0
govindraj-snippet <2023bit035@sggs.ac.in>
2026-01-06 17:19:38 +0530

    the getting the list of the urls for a partiucluar user functionlity is done

--- shortner/src/main/java/com/example/shortner/service/UrlMappingService.java
+++ shortner/src/main/java/com/example/shortner/service/UrlMappingService.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -49,5 +50,10 @@ public class UrlMappingService {
         }
         return shortUrl.toString();
 
+    }
+
+    public List<UrlMappingDTO> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream()
+                .map( this :: convertToDto).toList() ;
     }
 }
